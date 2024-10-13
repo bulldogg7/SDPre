d8fb596acfe9357298817a2d3152784840a95746
bulldogg7 <163910022+bulldogg7@users.noreply.github.com>
2024-10-12 22:15:38 -0600

    Objective2Lab3.java

---
+++ Labs/Objective2Lab3.java
@@ -0,0 +1,10 @@
+public class Objective2Lab3 {
+  public static void main(String[] args) {
+    int yearBorn = 1986;
+    String monthBorn = "December";
+    boolean weekend = false;
+    System.out.println(yearBorn);
+    System.out.println(monthBorn);
+    System.out.println(weekend);
+    }
+}

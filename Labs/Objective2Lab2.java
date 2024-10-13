aa50dfaa546c50aea1cf273c84f5234b9d8f2517
bulldogg7 <163910022+bulldogg7@users.noreply.github.com>
2024-10-12 20:45:53 -0600

    Objective2Lab2.java

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,9 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    String name = "Sheehan Thomson";
+    int age = 36;
+    String state = "California";
+    System.out.println("Name: " + name + " Age: " + age + " State: " + state);
+    
+  }
+}

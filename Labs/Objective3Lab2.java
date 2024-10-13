1a152b7b9618bd44cd46c2c6b719204c574933bf
bulldogg7 <163910022+bulldogg7@users.noreply.github.com>
2024-10-13 14:37:52 -0600

    Objective3Lab2.java

---
+++ Labs/Objective3Lab2.java
@@ -0,0 +1,10 @@
+public class Objective3Lab2 {
+public static void main(String[] args) {
+    double side1 = 20;
+    double side2 = 10;
+    double hypotenuse = Math.sqrt((side1 * side1) + (side2 * side2));
+    System.out.print("The hypotenuse of a triangle with sides ");
+    System.out.print(side1 + " and " + side2 + " is ");
+    System.out.println(hypotenuse);
+    }
+}
